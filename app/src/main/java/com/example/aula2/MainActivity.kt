94a4f46f7398c5fc3b10cd99eef183c19d61e5db
daniel6etec <daniel.rufino6@etec.sp.gov.br>
2023-09-06 14:37:21 -0300

    first commit

---
+++ app/src/main/java/com/example/aula2/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.aula2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aula2.ui.theme.Aula2Theme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Aula2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Daniel")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = Color.Magenta) {
+        Text(text = "Hi, my name is $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Aula2Theme {
+        Greeting("Android")
+    }
+}
